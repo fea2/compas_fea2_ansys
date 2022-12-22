965a3f6503c81261e034a0b136b84291d283fe1c
frankie <ranaudo@arch.ethz.ch>
2022-12-22 16:29:40 +0100

    fix imports

--- src/compas_fea2_ansys/model/materials/__init__.py
+++ src/compas_fea2_ansys/model/materials/__init__.py
@@ -2,17 +2,17 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from compas_fea2.backends.ansys.model.materials.concrete import AnsysConcrete
-from compas_fea2.backends.ansys.model.materials.concrete import AnsysConcreteDamagedPlasticity
-from compas_fea2.backends.ansys.model.materials.concrete import AnsysConcreteSmearedCrack
+from .concrete import AnsysConcrete
+from .concrete import AnsysConcreteDamagedPlasticity
+from .concrete import AnsysConcreteSmearedCrack
 
-from compas_fea2.backends.ansys.model.materials.material import AnsysElasticIsotropic
-from compas_fea2.backends.ansys.model.materials.material import AnsysElasticOrthotropic
-from compas_fea2.backends.ansys.model.materials.material import AnsysElasticPlastic
-from compas_fea2.backends.ansys.model.materials.material import AnsysStiff
-from compas_fea2.backends.ansys.model.materials.material import AnsysUserMaterial
+from .material import AnsysElasticIsotropic
+from .material import AnsysElasticOrthotropic
+from .material import AnsysElasticPlastic
+from .material import AnsysStiff
+from .material import AnsysUserMaterial
 
-from compas_fea2.backends.ansys.model.materials.steel import AnsysSteel
+from .steel import AnsysSteel
 
-from compas_fea2.backends.ansys.model.materials.timber import AnsysTimber
+from .timber import AnsysTimber
 
